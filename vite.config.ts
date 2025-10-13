844a956dffea7abc8717f200691e200e877103df
loverphp <loverphp487@gmail.com>
2025-10-13 12:19:27 +0300

    update vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'; // Import the plugin
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { browserslistToTargets } from 'lightningcss';
+import browserslist from 'browserslist';
 import path from 'path';
 
 // https://vite.dev/config/
@@ -17,5 +19,13 @@ export default defineConfig({
 	],
 	resolve: {
 		alias: { find: '@', replacement: path.resolve(__dirname, 'src') },
+	},
+	css: {
+		lightningcss: {
+			targets: browserslistToTargets(browserslist('>= 0.25%')), // Adjust target browsers as needed
+		},
+	},
+	build: {
+		cssMinify: 'lightningcss',
 	},
 });
